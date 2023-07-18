d02ae2e60156783b5fc0e7f25ae7ccbd26260491
Jules <110929073+juse122@users.noreply.github.com>
2023-07-18 09:09:54 +0200

    Fix pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,4 @@ export { Home } from "./Home";
 export { Movies } from "./Movies";
 export { MovieDetails } from "./MovieDetails";
 export { NotFound } from "./NotFound";
-export { Series } from "./Series";
-export { SeriesDetails } from "./SeriesDetails";
 export { Watchlist } from "./Watchlist";
